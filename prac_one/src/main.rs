bf5c05847351ff236844f0b417944bbc92bf523d
lastrucci01 <u20430168@tuks.co.za>
2023-02-26 00:13:00 +0200

    :bug: last time i swear

--- prac_one/src/main.rs
+++ prac_one/src/main.rs
@@ -54,7 +54,7 @@ fn default() -> String{
     <html>
       <head>
         <meta charset="UTF-8">./style.css">
-        <link rel="stylesheet" href="/style/script.css">
+        <link rel="stylesheet" href="/style.css">
         <title>COS 332 Prac One</title>
       </head>
       <body>
@@ -86,7 +86,7 @@ fn right() -> String {
   <html>
     <head>
       <meta charset="UTF-8">
-      <link rel="stylesheet" href="/style/script.css">
+      <link rel="stylesheet" href="/style.css">
       <title>COS 332 Prac One</title>
     </head>
     <body>
@@ -124,7 +124,7 @@ fn wrong() -> String{
   <html>
     <head>
       <meta charset="UTF-8">
-      <link rel="stylesheet" href="/style/script.css">
+      <link rel="stylesheet" href="/style.css">
       <title>COS 332 Prac One</title>
     </head>
     <body>
