8741c3b2c72a8c2a6cd7cfc0a70733538ae9f2bc
lastrucci01 <u20430168@tuks.co.za>
2023-03-24 22:45:32 +0200

    Untracked files

--- prac_3/src/main.rs
+++ prac_3/src/main.rs
@@ -1,3 +1,55 @@
+use reqwest::Error;
+use serde_json::Value;
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+use std::thread;
+
+#[derive(Debug)]
+struct TimeResponse {
+    abbreviation: String,
+    client_ip: String,
+    datetime: String,
+    day_of_week: u8,
+    day_of_year: u16,
+    dst: bool,
+    dst_from: Option<String>,
+    dst_offset: i32,
+    dst_until: Option<String>,
+    raw_offset: i32,
+    timezone: String,
+    unixtime: u64,
+    utc_datetime: String,
+    utc_offset: String,
+    week_number: u8,
+}
+
+fn handle_client(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+    let response = reqwest::blocking::get("http://worldtimeapi.org/api/ip").unwrap();
+    let time_response: Value = serde_json::from_str(&response.text().unwrap()).unwrap();
+    println!("Current time: {:?}", time_response);
+    let response = format!(
+        "HTTP/1.1 404 NOT FOUND\r\n\r\nCurrent time in seconds since Unix epoch: {:?}",
+        time_response
+    );
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:6969").unwrap();
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                thread::spawn(|| {
+                    handle_client(stream);
+                });
+            }
+            Err(e) => {
+                eprintln!("Error: {}", e);
+            }
+        }
+    }
 }
--- prac_3/target/debug/deps/prac_three-589ec093a92f4b5d.d
+++
@@ -1,5 +0,0 @@
-/Users/richardlastrucci/Library/CloudStorage/OneDrive-UniversityofPretoria/First Semester/COS 332/COS_332_Pracs/prac_three/target/debug/deps/prac_three-589ec093a92f4b5d.rmeta: src/main.rs
-
-/Users/richardlastrucci/Library/CloudStorage/OneDrive-UniversityofPretoria/First Semester/COS 332/COS_332_Pracs/prac_three/target/debug/deps/prac_three-589ec093a92f4b5d.d: src/main.rs
-
-src/main.rs:
